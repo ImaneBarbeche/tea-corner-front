a6e6efc655d96e3ee30c4f5b4f8f985a059cfb24
Imane <barbecheimane@gmail.com>
2026-03-09 16:11:21 +0100

    feature: created desktop nav

--- app/components/AppNav.tsx
+++ app/components/AppNav.tsx
@@ -10,22 +10,24 @@ export function AppNav() {
     { icon: "logs", link: "/app/logs", text: "Logs" },
     { icon: "dashboard", link: "/app/dashboard", text: "Dashboard" },
   ];
+
+  const settings = { icon: "settings", link: "/app/settings", text: "Settings" }
+
   return (
-    <nav className="px-6 py-5 bg-secondary-beige rounded-full shadow-card backdrop-blur-2xl mx-4 mb-2 fixed bottom-0 left-0 right-0">
-      <div className="">
-        <div className="hidden">
+    <nav className="px-6 py-5 bg-secondary-beige rounded-full shadow-card backdrop-blur-2xl mx-4 mb-2 fixed bottom-0 left-0 right-0 md:shadow-none md:top-0 md:bottom-0 md:right-auto md:bg-transparent md:p-0 md:m-0 md:pt-1.5">
+      <div className="md:grid md:grid-rows-[auto_1fr_auto] md:h-full md:items-start md:gap-10">
+        <div className="hidden md:block">
           <Logo />
         </div>
-        <ul className="flex justify-between">
+        <ul className="flex justify-between md:flex-col md:justify-between md:h-9/12 md:max-h-80">
             {navItems.map((item) => (
                 <li key={item.icon}>
                     <NavIcon {...item} />
                 </li>
             ))}
         </ul>
-        <NavLink to="" className="hidden">
-          Settings
-        </NavLink>
+        <NavIcon {...settings} className="hidden md:flex md:self-end" 
+        />
       </div>
     </nav>
   );
--- app/components/NavIcon.tsx
+++ app/components/NavIcon.tsx
@@ -4,8 +4,10 @@ import {
   Compass,
   ClipboardList,
   LayoutDashboard,
+  Settings,
 } from "lucide-react";
 import type { LucideIcon } from "lucide-react";
+import type { HTMLAttributes } from "react";
 import { NavLink } from "react-router";
 
 const iconMap: Record<string, LucideIcon> = {
@@ -14,29 +16,28 @@ const iconMap: Record<string, LucideIcon> = {
   discover: Compass,
   logs: ClipboardList,
   dashboard: LayoutDashboard,
+  settings: Settings,
 };
 
-interface NavIconProps {
+interface NavIconProps extends HTMLAttributes<HTMLElement> {
   icon: string;
   link: string;
   text: string;
   end?: boolean;
 }
 
-export function NavIcon({ icon, link, text, end }: NavIconProps) {
+export function NavIcon({ icon, link, text, end, ...props }: NavIconProps) {
   const Icon = iconMap[icon.toLowerCase()];
   return (
-    <NavLink to={link} end={end} className="flex flex-col items-center">
+    <NavLink to={link} end={end} className={`flex flex-col items-center ${props.className ?? ""}`}>
       {({ isActive }) => (
         <>
           <span
-            className={
-              `p-1.5 ${isActive ? "bg-primary-beige rounded-xl" : ""}`
-            }
+            className={`p-1.5 ${isActive ? "bg-primary-beige rounded-xl" : ""}`}
           >
             <Icon color="var(--color-primary-dark)" strokeWidth={1} size={36} />
           </span>
-          <span className="text-xs mt-3">{text}</span>
+          <span className="text-xs mt-3 md:hidden">{text}</span>
         </>
       )}
     </NavLink>
