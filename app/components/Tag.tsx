f1f11101b65d61c2763efea5140bebbbb2a959bb
Imane <barbecheimane@gmail.com>
2026-03-11 16:12:52 +0100

    fix: corrected width of tag to adapt to content

--- app/components/Tag.tsx
+++ app/components/Tag.tsx
@@ -34,7 +34,7 @@ export function Tag({ content, icon, color, ...props }: TagProps) {
   }
 
   return (
-    <div className="flex bg-primary-beige p-1.5 gap-1.5 rounded-full">
+    <div className="flex bg-primary-beige p-1.5 gap-1.5 rounded-full w-fit">
       <div
         className="flex items-center rounded-full p-1 bg-(--tag-color) min-w-6 aspect-square"
         style={
