92f43421af306736cc50dfcb894473b6861a04ff
GeorgeDaris <george.darisgr@gmail.com>
2026-03-13 16:30:14 +0100

    feat: added tertiary button variant

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,28 +1,30 @@
+import type { LucideIcon } from "lucide-react";
 import React, { type ButtonHTMLAttributes, type ReactElement } from "react";
 
 // allows the button to inherit standard button props (e.g type="submit")
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  variant?: "primary" | "secondary" | "ghost";
+  variant?: "primary" | "secondary" | "tertiary" | "ghost";
   size?: "small" | "medium" | "large";
   color?: "";
   disabled?: boolean;
-  icon?: ReactElement;
-  children: React.ReactNode;
+  icon?: LucideIcon;
+  children?: React.ReactNode;
 }
 
 export function Button({
   variant = "primary",
   size = "medium",
-  icon,
   children,
   className = "",
   disabled,
+  icon: Icon,
   ...props //spreading onClick etc
 }: ButtonProps) {
   const variants = {
     primary:
       "text-sm px-6 py-2.5 bg-primary-dark text-primary-light hover:bg-opacity-90",
     secondary: "border-2 border-primary-dark text-primary-dark bg-transparent",
+    tertiary: "text-primary-dark bg-primary-beige",
     ghost: "bg-transparent text-primary-dark",
   };
 
@@ -33,17 +35,22 @@ export function Button({
   };
 
   const baseStyles =
-    "inline-flex items-center justify-center rounded-full font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:opacity-70 disabled:cursor-not-allowed cursor-pointer";
+    "inline-flex items-center justify-center rounded-full font-medium transition-colors focus:outline-none focus-visible:ring-1 focus-visible:ring-offset-1 disabled:opacity-70 disabled:cursor-not-allowed cursor-pointer";
+
+  const isIconOnly = !children && !!Icon;
 
   return (
     <button
-      className={`${baseStyles} ${variants[variant]} ${sizes[size]} ${className}`}
+      className={`${baseStyles} ${variants[variant]} ${sizes[size]} ${isIconOnly ? "!px-2" : ""} ${className}`}
       disabled={disabled}
       aria-disabled={disabled}
       {...props}
     >
       {/* TODO: ICON & LINK */}
-      {children}
+
+      {Icon && <Icon size={16} className="" />}
+
+      {children && <span>{children}</span>}
     </button>
   );
 }
--- app/routes/app/Tea.tsx
+++ app/routes/app/Tea.tsx
@@ -1,18 +1,16 @@
 import type { Route } from "./+types/Tea";
 import { CONFIG } from "../../config";
 import { apiFetch } from "~/lib/api";
-import { TeaHeader } from "~/components/TeaHeader";
-import { TEA_TYPE_COLORS } from "./enums/teaType.enum";
-import type { Tea } from "~/types/tea";
-import type { Ingredient } from "~/types/ingredient";
-import { Tag } from "~/components/Tag";
-import type { TeaIngredient } from "~/types/TeaIngredient";
+import teaCup from "~/assets/images/tea-cup.png";
+
 import {
   formatBrewTime,
   formatLeafAmount,
   getTeaColor,
 } from "~/lib/teaFormatters";
 import { TeaInfoCol } from "~/components/TeaInfoCol";
+import type { Tea } from "~/types/tea";
+import { TeaTimer } from "~/components/TeaTimer";
 
 export async function clientLoader({ params }: Route.ClientLoaderArgs) {
   try {
@@ -55,54 +53,26 @@ export default function Tea({ loaderData }: Route.ComponentProps) {
 
   return (
     <section
-      className=""
+      className="flex flex-col md:flex-row items-center md:items-end md:justify-around w-full"
       style={{ "--tea-color": color } as React.CSSProperties}
     >
       <TeaInfoCol tea={tea} />
-      <div></div>
+      <div className="mt-8 md:mt-0 flex flex-col items-center gap-4">
+        <div
+          className="relative bg-
+          
+                after:content-['']  
+                after:absolute 
+                after:inset-0 
+                after:bg-[var(--tea-color)] 
+                after:mix-blend-hue 
+                after:pointer-events-none
+                after:z-20"
+        >
+          {/* <img src={teaCup} alt="a tea cup" className="max-w-72" /> */}
+        </div>
+        <TeaTimer seconds={tea.brewing_time} />
+      </div>
     </section>
-    // <section
-    //   className=""
-    //   style={{ "--tea-color": color } as React.CSSProperties}
-    // >
-    //   <div className="flex flex-col gap-4">
-    //     <TeaHeader tea={tea} />
-    //     {tea.description && (
-    //       <p className="max-w-80 text-justify">{tea.description}</p>
-    //     )}
-    //     {tea.ingredients.length > 0 && (
-    //       <div className="flex flex-col gap-2">
-    //         <p>Ingredients</p>
-    //         <div className="flex gap-2 flex-wrap">
-    //           {tea.ingredients.map(
-    //             ({ ingredient, quantity, optional }: TeaIngredient) => (
-    //               <Tag
-    //                 key={ingredient.id}
-    //                 content={`${ingredient.name} (${quantity}g)`}
-    //                 icon={ingredient.type}
-    //                 color={ingredient.color}
-    //               />
-    //             ),
-    //           )}
-    //         </div>
-    //       </div>
-    //     )}
-    //     <div className="flex flex-col gap-2">
-    //       <p>Caffeine</p>
-    //       <div className="flex gap-2 flex-wrap">
-    //         <p>{tea.caffeine_level}</p>
-    //       </div>
-    //     </div>
-    //     <div className="flex flex-col gap-2">
-    //       <p>Preparation</p>
-    //       <div className="flex gap-2 flex-wrap">
-    //         <p>{tea.brewing_temperature}°C</p>
-    //         <p>{formatBrewTime(tea.brewing_time)}</p>
-    //         <p>{tea.water_amount}ml</p>
-    //         {tea.leaf_amount && <p>{formatLeafAmount(tea.leaf_amount)}</p>}
-    //       </div>
-    //     </div>
-    //   </div>
-    // </section>
   );
 }
